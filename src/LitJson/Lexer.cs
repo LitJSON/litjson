47eacdc22f40dc3db043e1454d45e8207e924b58
Leonardo Boshell <leonardoboshell@users.sourceforge.net>
2007-08-19 01:38:23 -0500

    Added references to relevant parts of the manual from the source code

--- src/LitJson/Lexer.cs
+++ src/LitJson/Lexer.cs
@@ -132,6 +132,8 @@ namespace LitJson
 
         private static void PopulateFsmTables ()
         {
+            // See section A.1. of the manual for details of the finite
+            // state machine.
             fsm_handler_table = new StateHandler[28] {
                 State1,
                 State2,
