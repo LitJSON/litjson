6601f2bc0a1e19805c49c06b797fe2830a3f28cc
ZongJing Lu <sonic3d@gmail.com>
2013-05-08 16:54:02 +0800

    Add:MonoDevelop Solution/Project files

---
+++ build/MonoDevelop/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: CLSCompliant (true)]
+[assembly: AssemblyTitle("LitJson")]
+[assembly: AssemblyDescription("LitJSON library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LitJSON")]
+[assembly: AssemblyCopyright (
+    "The authors disclaim copyright to this source code")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("0.7.0")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
