d98f12ec6bb5e51d3f81f6573e02244912a4e341
Michael Bartnett <michael@bartnett.com>
2013-05-25 22:13:12 -0400

    Support nullable types, including primitives, enums and structs. Tests included. Nullable type support based on https://github.com/chipdude/litjson/commit/eaa84eed6e0d876b962c5b33b4fc4a01b4fdbd3b

--- src/LitJson/JsonMapper.cs
+++ src/LitJson/JsonMapper.cs
@@ -310,14 +310,17 @@ namespace LitJson
             if (reader.Token == JsonToken.ArrayEnd)
                 return null;
 
-            if (reader.Token == JsonToken.Null) {
+            Type underlying_type = Nullable.GetUnderlyingType(inst_type);
+            Type value_type = underlying_type ?? inst_type;
 
-                if (! inst_type.IsClass)
-                    throw new JsonException (String.Format (
+            if (reader.Token == JsonToken.Null) {
+                if (inst_type.IsClass || underlying_type != null) {
+                    return null;
+                }
+
+                throw new JsonException (String.Format (
                             "Can't assign null to an instance of type {0}",
                             inst_type));
-
-                return null;
             }
 
             if (reader.Token == JsonToken.Double ||
@@ -328,16 +331,16 @@ namespace LitJson
 
                 Type json_type = reader.Value.GetType ();
 
-                if (inst_type.IsAssignableFrom (json_type))
+                if (value_type.IsAssignableFrom (json_type))
                     return reader.Value;
 
                 // If there's a custom importer that fits, use it
                 if (custom_importers_table.ContainsKey (json_type) &&
                     custom_importers_table[json_type].ContainsKey (
-                        inst_type)) {
+                        value_type)) {
 
                     ImporterFunc importer =
-                        custom_importers_table[json_type][inst_type];
+                        custom_importers_table[json_type][value_type];
 
                     return importer (reader.Value);
                 }
@@ -345,20 +348,20 @@ namespace LitJson
                 // Maybe there's a base importer that works
                 if (base_importers_table.ContainsKey (json_type) &&
                     base_importers_table[json_type].ContainsKey (
-                        inst_type)) {
+                        value_type)) {
 
                     ImporterFunc importer =
-                        base_importers_table[json_type][inst_type];
+                        base_importers_table[json_type][value_type];
 
                     return importer (reader.Value);
                 }
 
                 // Maybe it's an enum
-                if (inst_type.IsEnum)
-                    return Enum.ToObject (inst_type, reader.Value);
+                if (value_type.IsEnum)
+                    return Enum.ToObject (value_type, reader.Value);
 
                 // Try using an implicit conversion operator
-                MethodInfo conv_op = GetConvOp (inst_type, json_type);
+                MethodInfo conv_op = GetConvOp (value_type, json_type);
 
                 if (conv_op != null)
                     return conv_op.Invoke (null,
@@ -411,11 +414,10 @@ namespace LitJson
                     instance = list;
 
             } else if (reader.Token == JsonToken.ObjectStart) {
+                AddObjectMetadata (value_type);
+                ObjectMetadata t_data = object_metadata[value_type];
 
-                AddObjectMetadata (inst_type);
-                ObjectMetadata t_data = object_metadata[inst_type];
-
-                instance = Activator.CreateInstance (inst_type);
+                instance = Activator.CreateInstance (value_type);
 
                 while (true) {
                     reader.Read ();
