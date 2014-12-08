68e5066d83a16c84900819eab9dd8edd62134144
Kim Hoang <github@aggressive-solutions.de>
2014-12-08 11:12:31 +0100

    added Visual Studio 2013 build option

---
+++ build/vs2013/litJSON/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("litJSON")]
+[assembly: AssemblyDescription("A .Net library to handle conversions from and to JSON (JavaScript Object Notation) strings.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("http://lbv.github.io/litjson/")]
+[assembly: AssemblyProduct("litJSON")]
+[assembly: AssemblyCopyright("Unlicense (public domain)")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6e7b59a1-67ad-4e29-8851-a5c9626a7657")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
